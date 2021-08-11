735790438e0dbca3811aec51fcc9425050e863a1
Joel Auerbach <joelauerbach1@gmail.com>
2021-08-11 15:35:03 -0700

    Create tags option on the list page

--- functions.php
+++ functions.php
@@ -3,50 +3,54 @@
 
 // Define metadata fields for media uploads
 function media_metadata( $form_fields, $post ) {
-	$form_fields['video-title'] = array(
-		'label' => 'Video Title',
-		'input' => 'text',
-		'value' => get_post_meta( $post->ID, 'video_title', true )
-	);
+    $form_fields['video-title'] = array(
+        'label' => 'Video Title',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'video_title', true )
+    );
 
-	$form_fields['canonical-url'] = array(
-		'label' => 'Canonical URL',
-		'input' => 'text',
-		'value' => get_post_meta( $post->ID, 'canonical_url', true )
-	);
+    $form_fields['canonical-url'] = array(
+        'label' => 'Canonical URL',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'canonical_url', true )
+    );
 
-	$form_fields['permission-status'] = array(
-		'label' => 'Permission Status',
-		'input' => 'text',
-		'value' => get_post_meta( $post->ID, 'permission_status', true )
-	);
+    $form_fields['permission-status'] = array(
+        'label' => 'Permission Status',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'permission_status', true )
+    );
 
-	$form_fields['thumbnail-url'] = array(
-		'label' => 'Thumbnail URL',
-		'input' => 'text',
-		'value' => get_post_meta( $post->ID, 'thumbnail_url', true )
-	);
+    $form_fields['thumbnail-url'] = array(
+        'label' => 'Thumbnail URL',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'thumbnail_url', true )
+    );
 
-	return $form_fields;
+    return $form_fields;
 }
 
 add_filter( 'attachment_fields_to_edit', 'media_metadata', 10, 2 );
+add_filter( 'auto_update_theme', '__return_true' );
+add_filter( 'auto_update_plugin', '__return_true' );
+add_filter( 'auto_update_translation', '__return_true' );
+apply_filters( 'auto_core_update_send_email', '__return_true' );
 
 function save_media_metadata( $post, $attachment ) {
 
-	error_log($post['post_title']);
+    error_log($post['post_title']);
 
-	if( isset( $attachment['video-title'] ) ) {
+    if( isset( $attachment['video-title'] ) ) {
         update_post_meta( $post['ID'], 'video_title', $attachment['video-title'] );
-	};
+    };
 
     if( isset( $attachment['canonical-url'] ) )
-		update_post_meta( $post['ID'], 'canonical_url', esc_url( $attachment['canonical-url'] ) );
+        update_post_meta( $post['ID'], 'canonical_url', esc_url( $attachment['canonical-url'] ) );
 
     if( isset( $attachment['permission-status'] ) )
         update_post_meta( $post['ID'], 'permission_status', $attachment['permission-status'] );
 
-	return $post;
+    return $post;
 }
 add_filter( 'attachment_fields_to_save', 'save_media_metadata', 10, 2 );
 
@@ -62,11 +66,11 @@ function video_title_value( $column_name, $id ) {
     $meta = get_post_meta($id);
 
     if ( isset( $meta['video_title'])) {
-    	$value = implode('', $meta['video_title']);
+        $value = implode('', $meta['video_title']);
     } elseif ( isset($meta['_wp_attached_file'])) {
-    	$value = explode('/', $meta['_wp_attached_file'][0])[2];
+        $value = explode('/', $meta['_wp_attached_file'][0])[2];
     } else {
-    	$value = 'untitled';
+        $value = 'untitled';
     }
     echo $value;
 }
@@ -88,27 +92,27 @@ add_action("add_meta_boxes", "add_entry_meta_box");
 
 function entry_metadata( $post )
 {
-	$form_fields = array();
+    $form_fields = array();
 
-	$form_fields['text_excerpt'] = 'Text Excerpt';
+    $form_fields['text_excerpt'] = 'Text Excerpt';
 
-	$form_fields['author_first_name'] = 'Author First Name';
+    $form_fields['author_first_name'] = 'Author First Name';
 
-	$form_fields['author_last_name'] = 'Author Last Name';
+    $form_fields['author_last_name'] = 'Author Last Name';
 
-	$form_fields['additional_authors'] = 'Additional Authors (comma-separated)';
+    $form_fields['additional_authors'] = 'Additional Authors (comma-separated)';
 
-	$form_fields['author_website'] = 'Author Website';
+    $form_fields['author_website'] = 'Author Website';
 
-	$form_fields['additional_author_websites'] = 'Additional Author Websites (comma-separated; same order as additional authors)';
+    $form_fields['additional_author_websites'] = 'Additional Author Websites (comma-separated; same order as additional authors)';
 
-	$form_fields['author_bio'] = 'Author Bio';
+    $form_fields['author_bio'] = 'Author Bio';
 
-	$form_fields['citation'] = 'Citation';
+    $form_fields['citation'] = 'Citation';
 
-	// $form_fields['feature_in_homepage_slideshow'] = 'Feature in Homepage Slideshow';
+    // $form_fields['feature_in_homepage_slideshow'] = 'Feature in Homepage Slideshow';
 
-	return $form_fields;
+    return $form_fields;
 }
 
 function entry_meta_box_markup($post)
@@ -119,21 +123,21 @@ function entry_meta_box_markup($post)
 
     // $feature = get_post_meta($post->ID, 'feature_in_homepage_slideshow', true);
     // if (empty($feature)) {
-    // 	update_post_meta($post->ID, 'feature_in_homepage_slideshow', 'No');
+    //  update_post_meta($post->ID, 'feature_in_homepage_slideshow', 'No');
     // }
 
     foreach ( array_keys($fields) as $field ) {
 
-    	$label = $fields[$field];
+        $label = $fields[$field];
 
-    	?>
-		    <div>
-		        <label for="<?php echo $field; ?>"><?php echo $label; ?></label>
-		        <br>
-		        <textarea name="<?php echo $field; ?>" rows="2" cols="60"><?php echo htmlentities(get_post_meta($post->ID, $field, true)); ?></textarea>
-		    </div>
-		    <br>
-    	<?php
+        ?>
+            <div>
+                <label for="<?php echo $field; ?>"><?php echo $label; ?></label>
+                <br>
+                <textarea name="<?php echo $field; ?>" rows="2" cols="60"><?php echo htmlentities(get_post_meta($post->ID, $field, true)); ?></textarea>
+            </div>
+            <br>
+        <?php
     }
   
 }
@@ -157,13 +161,13 @@ function save_entry_meta_box($post_id, $post, $update)
 
     foreach ( array_keys($fields) as $field ) {
 
-    	$value = "";
+        $value = "";
 
-		if(isset($_POST[$field])) {
-			$value = $_POST[$field];
-		};
+        if(isset($_POST[$field])) {
+            $value = $_POST[$field];
+        };
 
-		update_post_meta($post_id, $field, $value);
+        update_post_meta($post_id, $field, $value);
     }
 }
 
@@ -171,8 +175,8 @@ add_action("save_post", "save_entry_meta_box", 10, 3);
 
 // Custom category metadata
 function add_category_metadata( $term ) {
-	if (current_filter() == 'category_edit_form_fields') {
-		$category_author = get_term_meta($term->term_id, 'category_author', true);
+    if (current_filter() == 'category_edit_form_fields') {
+        $category_author = get_term_meta($term->term_id, 'category_author', true);
         $post_featured_on_homepage = get_term_meta($term->term_id, 'post_featured_on_homepage', true);
         ?>
         <tr class="form-field">
@@ -188,7 +192,7 @@ function add_category_metadata( $term ) {
                 <p class="description"><?php _e('Please enter the URL of a post'); ?></p>
             </td>
         </tr>
-	<?php } elseif (current_filter() == 'category_add_form_fields') {
+    <?php } elseif (current_filter() == 'category_add_form_fields') {
         ?>
         <div class="form-field">
             <label for="term_fields[category_author]"><?php _e('Category Description Author'); ?></label>
@@ -207,7 +211,7 @@ add_filter( 'category_add_form_fields', 'add_category_metadata', 10, 2 );
 add_filter( 'category_edit_form_fields', 'add_category_metadata', 10, 2 );
 
 function save_category_metadata( $term_id ) {
-	if (!isset($_POST['term_fields'])) {
+    if (!isset($_POST['term_fields'])) {
         return;
     }
 
@@ -220,16 +224,16 @@ add_filter( 'edited_category', 'save_category_metadata', 10, 2 );
 add_filter( 'created_category', 'save_category_metadata', 10, 2 );
 
 function admin_filter_author(){
-	remove_filter('parse_query', 'filter_posts_by_author');
+    remove_filter('parse_query', 'filter_posts_by_author');
     $posts = get_posts(array('numberposts' => -1));
     foreach ($posts as $post) {
-    	$post->author_last_name = get_post_meta($post->ID, 'author_last_name', true);
-		$post->parsed_author = parse_author($post, $the_list=true);
+        $post->author_last_name = get_post_meta($post->ID, 'author_last_name', true);
+        $post->parsed_author = parse_author($post, $the_list=true);
     };
-	
-	function cmp($a, $b) {
-		return strcasecmp($a->author_last_name, $b->author_last_name);
-	}
+    
+    function cmp($a, $b) {
+        return strcasecmp($a->author_last_name, $b->author_last_name);
+    }
     usort($posts, 'cmp');
 
     $keys = array_column($posts, 'author_last_name');
@@ -269,43 +273,43 @@ add_filter( 'parse_query', 'filter_posts_by_author' );
 
 // Load style sheet and fonts
 function load_styles() {
-	wp_register_style( 'custom_style', get_stylesheet_uri() );
-	wp_enqueue_style( 'custom_style' );
-	wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Space+Mono:400,700', false );
-	wp_enqueue_style( 'custom-google-fonts-2', 'https://fonts.googleapis.com/css?family=Work+Sans', false );
-	// Remove margin-top reserved for footer when wp_head() is called
-	remove_action('wp_head', '_admin_bar_bump_cb');
+    wp_register_style( 'custom_style', get_stylesheet_uri() );
+    wp_enqueue_style( 'custom_style' );
+    wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Space+Mono:400,700', false );
+    wp_enqueue_style( 'custom-google-fonts-2', 'https://fonts.googleapis.com/css?family=Work+Sans', false );
+    // Remove margin-top reserved for footer when wp_head() is called
+    remove_action('wp_head', '_admin_bar_bump_cb');
 }
 add_action( 'wp_enqueue_scripts', 'load_styles' );
 
 function get_thumbnail( $post ) {
-	$attachments = get_posts(array('post_type' => 'attachment',
-							      'post_parent' => $post->ID));
-	if (!empty($attachments)) {
-		$attachment = $attachments[0]->ID;
-		return get_post_meta($attachment)['thumbnail_url'][0];
-	}
-	else {
-		return get_site_icon_url();
-	}
+    $attachments = get_posts(array('post_type' => 'attachment',
+                                  'post_parent' => $post->ID));
+    if (!empty($attachments)) {
+        $attachment = $attachments[0]->ID;
+        return get_post_meta($attachment)['thumbnail_url'][0];
+    }
+    else {
+        return get_site_icon_url();
+    }
 }
 
 function get_attachment_url_by_title( $title ) {
-	$attachment = get_page_by_title($title, OBJECT, 'attachment');
-	  if ( $attachment ){
-	    $attachment_url = $attachment->guid;
-	  } 
-	  else {
-	    return 'image-not-found';
-	  }
-	  return $attachment_url;
+    $attachment = get_page_by_title($title, OBJECT, 'attachment');
+      if ( $attachment ){
+        $attachment_url = $attachment->guid;
+      } 
+      else {
+        return 'image-not-found';
+      }
+      return $attachment_url;
 }
 
 // Get featured post from category on homepage
 function get_featured_post ( $category ) {
-	$meta = get_term_meta($category->term_id);
-	$url = $meta['post_featured_on_homepage'][0];
-	return get_post(url_to_postid($url));
+    $meta = get_term_meta($category->term_id);
+    $url = $meta['post_featured_on_homepage'][0];
+    return get_post(url_to_postid($url));
 }
 
 // Wrap oembeds in a video-container
@@ -316,37 +320,37 @@ add_filter('embed_oembed_html', 'wrap_oembed', 99, 4);
 
 // Remove related videos from youtube embeds
 function remove_youtube_rel( $cache, $url, $attr, $post_ID ) {
-	
-	if ( strstr( $cache, 'youtube.com/embed/' ) ) {
-		$cache = str_replace( '?feature=oembed', '?feature=oembed&rel=0', $cache );
-	}
-	
-	return $cache;
+    
+    if ( strstr( $cache, 'youtube.com/embed/' ) ) {
+        $cache = str_replace( '?feature=oembed', '?feature=oembed&rel=0', $cache );
+    }
+    
+    return $cache;
 }
 add_filter( 'embed_oembed_html', 'remove_youtube_rel', 10, 4 );
 
 // Maintain state for currently "active" category
 function session_cat($cat=null, $post_id=null, $reset=false) {
-	// set active to $cat if passed
-	if (isset($cat)) {
-		$_SESSION['active_category'] = get_cat_name($cat);
-	}
-	// if post passed...
-	elseif (isset($post_id)) {
-		$categories = get_the_category($post_id);
-		// if the post has multiple categories and one of these is already current, keep that one
-		if (count($categories) > 1 && isset($_SESSION['active_category']) && in_array($_SESSION['active_category'], array_column($categories, 'name'))) {
-			return $_SESSION['active_category'];
-		}
-		// else set to first category of post
-		$_SESSION['active_category'] = get_the_category($post_id)[0]->name;
-	}
-	// initialize if no parameters passed and (not set or reset is true)
-	elseif (!isset($_SESSION) || !isset($_SESSION['active_category']) || $reset===true) {
-		$_SESSION['active_category'] = null;
-	}
-	// else, pass through (and always return value)
-	return $_SESSION['active_category'];
+    // set active to $cat if passed
+    if (isset($cat)) {
+        $_SESSION['active_category'] = get_cat_name($cat);
+    }
+    // if post passed...
+    elseif (isset($post_id)) {
+        $categories = get_the_category($post_id);
+        // if the post has multiple categories and one of these is already current, keep that one
+        if (count($categories) > 1 && isset($_SESSION['active_category']) && in_array($_SESSION['active_category'], array_column($categories, 'name'))) {
+            return $_SESSION['active_category'];
+        }
+        // else set to first category of post
+        $_SESSION['active_category'] = get_the_category($post_id)[0]->name;
+    }
+    // initialize if no parameters passed and (not set or reset is true)
+    elseif (!isset($_SESSION) || !isset($_SESSION['active_category']) || $reset===true) {
+        $_SESSION['active_category'] = null;
+    }
+    // else, pass through (and always return value)
+    return $_SESSION['active_category'];
 }
 
 function start_session() {
@@ -357,33 +361,33 @@ function start_session() {
 add_action('init', 'start_session', 1);
 
 function get_published_posts() {
-	$args = array('post_status' => 'publish', 'numberposts' => -1);
-	$published_posts = get_posts($args);
-	return $published_posts;
+    $args = array('post_status' => 'publish', 'numberposts' => -1);
+    $published_posts = get_posts($args);
+    return $published_posts;
 }
 
 // Format author name for display
 function parse_author($post, $the_list=false) {
 
     if ($the_list===true) {
-    	if ($post->author_first_name != '') {
-    		$author = "{$post->author_last_name}, {$post->author_first_name}";
-    	}
-    	else {
-    		$author = "{$post->author_last_name}";
-    	}
-    	$sep = ', & ';
+        if ($post->author_first_name != '') {
+            $author = "{$post->author_last_name}, {$post->author_first_name}";
+        }
+        else {
+            $author = "{$post->author_last_name}";
+        }
+        $sep = ', & ';
     }
     else {
-    	$author = "{$post->author_first_name} {$post->author_last_name}";
-    	$sep = ' and ';
+        $author = "{$post->author_first_name} {$post->author_last_name}";
+        $sep = ' and ';
     };
 
     if (!empty($post->additional_authors)) {
         $aa = explode(',', $post->additional_authors);
 
         for ($i = 0; $i < count($aa) - 1; $i++) {
-        	$a = trim($aa[$i]);
+            $a = trim($aa[$i]);
             $author .= ", {$a}";
         }
 
@@ -395,22 +399,26 @@ function parse_author($post, $the_list=false) {
 }
 
 function load_scripts() {
-	wp_enqueue_script( 'hamburger', get_template_directory_uri() . '/scripts/hamburger.js', array( 'jquery' ) );
-	wp_enqueue_script( 'the-list-toggle', get_template_directory_uri() . '/scripts/the-list-toggle.js', array( 'jquery' ) );
+    wp_enqueue_script( 'hamburger', get_template_directory_uri() . '/scripts/hamburger.js', array( 'jquery' ) );
+    wp_enqueue_script( 'the-list-toggle', get_template_directory_uri() . '/scripts/the-list-toggle.js', array( 'jquery' ) );
 
-	$published_posts = get_published_posts();
-	
-	foreach ( $published_posts as $post ) {
-		$post->url = get_permalink($post);
-		$category = get_the_category($post->ID)[0];
-		$post->category_name = $category->name;
-		$post->category_url = get_category_link($category->term_id);
-		$post->author_last_name = get_post_meta($post->ID, 'author_last_name', true);
-		$post->parsed_author = parse_author($post, $the_list=true);
-	};
+    $published_posts = get_published_posts();
+    
+    foreach ( $published_posts as $post ) {
+        $post->url = get_permalink($post);
+        $category = get_the_category($post->ID)[0];
+        $post->category_name = $category->name;
+        $post->category_url = get_category_link($category->term_id);
+        $post->author_last_name = get_post_meta($post->ID, 'author_last_name', true);
+        $post->parsed_author = parse_author($post, $the_list=true);
+    };
 
-	wp_localize_script( 'the-list-toggle', 'published_posts', $published_posts );
-	// wp_enqueue_script( 'slideshow', get_template_directory_uri() . '/scripts/slideshow.js', array( 'jquery' ) );
+    wp_localize_script( 'the-list-toggle', 'published_posts', $published_posts );
+    
+    $all_tags = get_tags();
+    wp_localize_script( 'the-list-toggle', 'all_tags', $all_tags );
+    
+    // wp_enqueue_script( 'slideshow', get_template_directory_uri() . '/scripts/slideshow.js', array( 'jquery' ) );
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
